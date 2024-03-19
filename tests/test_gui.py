2d86e181591f43119c151a34850f6568f11705cf
giuliarizzo9 <151658627+giuliarizzo9@users.noreply.github.com>
2024-03-19 16:49:39 +0100

    change to test_guy.py

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -36,3 +36,29 @@ class TestExpressions(CalculatorGUITestCase):
         self.assert_display("1.2+2")
         self.press_button("=")
         self.assert_display("3.2")
+
+# EXERCISE:
+
+class CalculatorGUITestCase(unittest.TestCase):
+    # rest of the class is unchanged 
+
+    def assert_button_exists(self, button_text):
+        self.assertIsNotNone(self.app.find_button_by(button_text))
+
+
+class TestLayout(CalculatorGUITestCase):
+    buttons_to_test = {
+        'C',
+        '7', '8', '9', '/',
+        '4', '5', '6', '*',
+        '1', '2', '3', '-',
+        '.', '0', '=', '+',
+    }
+
+    def test_initial_display(self):
+        self.assert_display("0")
+
+    def test_buttons(self):
+        for button_text in self.buttons_to_test:
+            with self.subTest(button=button_text):
+                self.assert_button_exists(button_text)
